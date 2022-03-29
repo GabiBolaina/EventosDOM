56df6b1863f848116aca479dfb8e8be6e009ae7e
GABRIELA MARIA ROCHA BOLAINA <gabibolaina@gmail.com>
2022-03-29 15:20:25 -0300

    Eventos Dom
    Inicio

---
+++ _javascript/funcoes.js
@@ -0,0 +1,13 @@
+function clicar(){
+a.innerText = 'Você clicou!'
+a.style.background = 'Red'
+};
+        
+function entrar(){
+a.innerText = ('Mouse entrou na área!')
+};
+        
+function sair(){
+a.innerText = ('Mouse saiu da área!')
+a.style.background = 'rgb(42, 139, 42)'
+};
